df64a64192a1c8d9be3f7ffea3f2a13640144a4e
Pedro26fer <p2698fernandes@gmail.com>
2025-07-09 21:35:35 -0300

    feat: user service developed

--- backend/src/users/user.service.ts
+++ backend/src/users/user.service.ts
@@ -1,59 +1,125 @@
-import * as bcrypt from 'bcryptjs'
-import {Injectable, NotFoundException, ConflictException, Param} from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
-import { CreateUserDTO } from './dto/createUser.dto'
-import { User } from './user.entity'
+import * as bcrypt from 'bcryptjs';
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+  Param,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDTO } from './dto/createUser.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService{
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>
-    ){}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-    async create(createdUserDto: CreateUserDTO): Promise<User>{
+  async create(createdUserDto: CreateUserDTO): Promise<User> {
+    const isThereThisUser = await this.userRepository.findOne({
+      where: { email: createdUserDto.email },
+    });
 
-        const isThereThisUser = await this.userRepository.findOne({where: {email: createdUserDto.email}})
-
-        if(isThereThisUser){
-            throw new ConflictException('#EMAIL JÁ UTILIZADO')
-        }
-
-        const saltsHash = 10
-        const hashedPassowrd = await bcrypt.hash(createdUserDto.password, saltsHash)
-
-        const user = this.userRepository.create({
-            ...createdUserDto,
-            password: hashedPassowrd
-        })
-
-        const savedUser = await this.userRepository.save(user)
-
-        const {password, ...result} = savedUser
-        return result as User
+    if (isThereThisUser) {
+      throw new ConflictException('#EMAIL JÁ UTILIZADO');
     }
 
-    async findAll(): Promise<User[]>{
-        const users = await this.userRepository.find({
-            select:['id', 'email', 'name', 'role', 'isActive', 'createdAt', 'updatedAt'],
-            order: {createdAt: 'DESC'}
-        })
-        return users
+    const saltsHash = 10;
+    const hashedPassowrd = await bcrypt.hash(
+      createdUserDto.password,
+      saltsHash,
+    );
+
+    const user = this.userRepository.create({
+      ...createdUserDto,
+      password: hashedPassowrd,
+    });
+
+    const savedUser = await this.userRepository.save(user);
+
+    const { password, ...result } = savedUser;
+    return result as User;
+  }
+
+  async findAll(): Promise<User[]> {
+    const users = await this.userRepository.find({
+      select: [
+        'id',
+        'email',
+        'name',
+        'role',
+        'isActive',
+        'createdAt',
+        'updatedAt',
+      ],
+      order: { createdAt: 'DESC' },
+    });
+    return users;
+  }
+
+  async findById(id: string): Promise<User> {
+    const foundUser = await this.userRepository.findOne({ where: { id } });
+
+    if (!foundUser) {
+      throw new NotFoundException();
+    }
+    return foundUser;
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const foundUser = await this.userRepository.findOne({ where: { email } });
+
+    if (!foundUser) {
+      throw new NotFoundException();
+    }
+    return foundUser;
+  }
+
+  async updateStatusUser(id: string, isActive: boolean) {
+    const user = await this.findById(id);
+
+    user.isActive = isActive;
+    const updatedUserStatus = await this.userRepository.save(user);
+
+    const { password, ...result } = updatedUserStatus;
+    return result as User;
+  }
+
+  async removeUser(id: string): Promise<void> {
+    const userToRemove = await this.userRepository.findOne({ where: { id } });
+
+    if (!userToRemove) {
+      throw new NotFoundException();
     }
 
-    async findById(id: string): Promise<User>{
-        const foundUser = await this.userRepository.findOne({where: {id}})
+    await this.userRepository.remove(userToRemove);
+  }
 
-        if(!foundUser){
-            throw new NotFoundException
-        }
+  async getUsersStats() {
+    const totalUsers = await this.userRepository.count();
+    const activeUsers = await this.userRepository.count({
+      where: {
+        isActive: true,
+      },
+    });
 
-        return foundUser
-    }
+    const usersByRole = await this.userRepository
+      .createQueryBuilder('users')
+      .select('users.role', 'role')
+      .addSelect('COUNT(*)', 'count')
+      .groupBy('user.role')
+      .getRawMany();
 
-    
- 
-    
-
+    return {
+      totalUsers,
+      activeUsers,
+      inactiveUser: totalUsers - activeUsers,
+      usersByRole: usersByRole.reduce((acc, item) => {
+        acc[item.role] = parseInt(item.count);
+        return acc;
+      }, {}),
+    };
+  }
 }
---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,5 @@
+// import { UserService } from "./user.service";
+// import { CreateUserDTO } from "./dto/createUser.dto";
+
+// import {Controller, Get, Post, Delete, Patch, Param, UseGuards, Request } from '@nestjs/common'
+
