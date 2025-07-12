5a3a54ff00063e6df06545efe208937e20dc627c
Pedro26fer <p2698fernandes@gmail.com>
2025-07-11 22:23:49 -0300

    feat: Serviço de autenticação

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,34 +1,80 @@
-// import { Injectable, UnauthorizedException } from '@nestjs/common';
-// import { JwtService } from '@nestjs/jwt';
-// import { UsersService } from '../users/users.service';
-// import { LoginDto } from './dto/login.dto';
-// import * as bcrypt from 'bcryptjs';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/users/user.service';
+import { LoginDTO } from './dto/login.dto';
+import * as bcrypt from 'bcryptjs';
+import { User } from 'src/users/user.entity';
 
-// export interface JWTPayload {
-//   userId: string;
-//   email: string;
-//   role: string;
-//   name: string;
-// }
+export interface JWTPayload {
+  userId: string;
+  email: string;
+  role: string;
+  name: string;
+}
 
-// export interface LoginResponse {
-//   accsess_token : string;
-//   user: {
-//     id: string;
-//     name: string;
-//     email: string;
-//     role: string;
+export interface LoginResponse {
+  accsess_token: string;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+  };
+}
 
-//   }
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-// }
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findByEmail(email);
 
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
 
-// @Injectable()
-// export class AuthService {
+  async login(loginDto: LoginDTO): Promise<LoginResponse> {
+    const user = await this.validateUser(loginDto.email, loginDto.password);
 
-//   constructor(
-//     private userService: UserService;
-//     private jwtService: JwtService
-//   )
-// }
+    if (!user) {
+      throw new UnauthorizedException('Credenciais Inválidas');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('Usuário inativo');
+    }
+
+    const payload: JWTPayload = {
+      userId: user.id,
+      email: user.email,
+      name: user.name,
+      role: user.role,
+    };
+
+    return {
+      accsess_token: this.jwtService.sign(payload),
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+      },
+    };
+  }
+
+  async validateToken(jwtPayload: JWTPayload): Promise<User> {
+    const user = await this.userService.findById(jwtPayload.userId);
+
+    if (!user || !user.isActive) {
+      throw new UnauthorizedException('Token inválido');
+    }
+
+    return user;
+  }
+}
